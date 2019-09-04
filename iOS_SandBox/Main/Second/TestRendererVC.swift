18957a158ea0ecdc21affa613777cbc132c13fa8
ChocOZerO <nayalth@gmail.com>
2019-09-04 16:40:47 +0900

    2depth 뷰 적용

--- iOS_SandBox/Main/Second/TestRendererVC.swift
+++ iOS_SandBox/Main/Second/TestRendererVC.swift
@@ -40,11 +40,35 @@ class TestRendererVC: UIViewController, VCInitializer {
                 })
             )
         ) {
+            $0.style.placer = InlinePlacer
+            $0.style.width.value  = "auto"
+            $0.style.height.value = "auto"
+        }
+        let item0_1 = also(
+            Renderer.Item(
+                renderer: ViewRenderer(target: also(UIView()) {
+                    $0.backgroundColor = UIColor(red: 1, green: 0, blue: 1, alpha: 1.0)
+                })
+            )
+        ) {
             $0.style.width.value  = 200
             $0.style.height.value = 200
             $0.style.backgroundColor.value = "#ffff00"
         }
+        let item0_2 = also(
+            Renderer.Item(
+                renderer: ViewRenderer(target: also(UIView()) {
+                    $0.backgroundColor = UIColor(red: 0, green: 1, blue: 1, alpha: 1.0)
+                })
+            )
+        ) {
+            $0.style.width.value  = 200
+            $0.style.height.value = 200
+        }
         root.addItem(item: item0)
+        item0.addItem(item: item0_1)
+        item0.addItem(item: item0_2)
+        item0.reflow()
         let item1 = also(
             Renderer.Item(
                 renderer: ViewRenderer(target: also(UIView()) {
@@ -65,19 +89,28 @@ class TestRendererVC: UIViewController, VCInitializer {
             dsl.isInfinity = true
             dsl.block = { item in
                 count += 1
-                if count % Int(t) == 0 {
-                    UIView.animate(withDuration: 15 * t / 1000) {
-                        item0.style.width.value  = Double.random(in: 0...200) + 100
-                        item0.style.height.value = Double.random(in: 0...200) + 100
-                        item0.style.backgroundColor.value = { () -> String in
+                if count % 40 == 0 {
+
+                    UIView.animate(withDuration: 0.3) {
+                        item0_1.style.width.value  = Double.random(in: 0...200) + 30
+                        item0_1.style.height.value = Double.random(in: 0...200) + 30
+                        item0_1.style.backgroundColor.value = { () -> String in
                             let r = String(Int.random(in: 0...155) + 100, radix: 16)
                             let g = String(Int.random(in: 0...155) + 100, radix: 16)
                             let b = String(Int.random(in: 0...155) + 100, radix: 16)
                             return "#\(r)\(g)\(b)"
                         }()
-                        item1.style.width.value  = Double.random(in: 0...200) + 100
-                        item1.style.height.value = Double.random(in: 0...200) + 100
-                        item0.reflow()
+                        item0_2.style.width.value  = Double.random(in: 0...200) + 30
+                        item0_2.style.height.value = Double.random(in: 0...200) + 30
+                        item0_2.style.backgroundColor.value = { () -> String in
+                            let r = String(Int.random(in: 0...155) + 100, radix: 16)
+                            let g = String(Int.random(in: 0...155) + 100, radix: 16)
+                            let b = String(Int.random(in: 0...155) + 100, radix: 16)
+                            return "#\(r)\(g)\(b)"
+                        }()
+                        item1.style.width.value  = Double.random(in: 0...200) + 50
+                        item1.style.height.value = Double.random(in: 0...200) + 50
+                        item0_1.reflow()
                     }
                     self.containerH.constant = CGFloat(root.offset.h)
                     UIView.animate(withDuration: 15 * t / 1000) {
